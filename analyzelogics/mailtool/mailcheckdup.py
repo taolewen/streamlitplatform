d784d32f7779ea315d72986f8ba62cbde8bd4fe4
taolewen <1352518735@qq.com>
2023-07-05 11:08:10 +0800

    qw

--- analyzelogics/mailtool/mailcheckdup.py
+++ analyzelogics/mailtool/mailcheckdup.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import pymysql
 
 from dbs import mysqlconn
+import streamlit as st
 
 
 def check_dup(mailaddress,status=0,name='test'):
@@ -8,11 +10,17 @@ def check_dup(mailaddress,status=0,name='test'):
     print(df)
     print(len(df))
     if len(df)==0:
+        mysqlconn1 = pymysql.connect(host=st.secrets["mysql"]['host'],
+                                    user=st.secrets["mysql"]['user'],
+                                    password=st.secrets["mysql"]['password'],
+                                    db=st.secrets["mysql"]['database'])
         status='未回复'
         sql=f''' insert into email_check (mailaddress,status,name) values ('{mailaddress}','{status}','{name}') '''
-        cursor=mysqlconn.cursor()
+        cursor=mysqlconn1.cursor()
         cursor.execute(sql)
         mysqlconn.commit()
+        cursor.close()
+        mysqlconn1.close()
         return 0,df
     else:
         return 1,df
@@ -20,8 +28,13 @@ def check_dup(mailaddress,status=0,name='test'):
 
 
 def update_status(mailaddress,status):
+    mysqlconn2 = pymysql.connect(host=st.secrets["mysql"]['host'],
+                                 user=st.secrets["mysql"]['user'],
+                                 password=st.secrets["mysql"]['password'],
+                                 db=st.secrets["mysql"]['database'])
     sql=f'''update email_check set status='{status}' where mailaddress='{mailaddress}' '''
-    cursor = mysqlconn.cursor()
+    cursor = mysqlconn2.cursor()
     cursor.execute(sql)
     mysqlconn.commit()
-
+    cursor.close()
+    mysqlconn2.close()
