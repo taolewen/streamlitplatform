bfaf100f3591182f2c71ee5900c5701fac3ce156
taolewen <1352518735@qq.com>
2023-07-06 11:05:56 +0800

    qw

--- pages/2_📈_emailcheck.py
+++ pages/2_📈_emailcheck.py
@@ -47,4 +47,21 @@ with tab1:
             update_status(email_input,status_option)
             st.experimental_rerun()
 with tab2:
-    st.dataframe(get_data())
+    df=get_data()
+    st.dataframe(df)
+
+
+    @st.cache_data
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+
+
+    csv = convert_df(df)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='email_list.csv',
+        mime='text/csv',
+    )
